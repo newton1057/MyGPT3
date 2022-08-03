5d30bc6e392cd32ad43b5538c8ce3039eaae7be5
newton1057Laptop <newton1057@gmail.com>
2022-08-02 22:04:16 -0500

    Añadir patch

--- src/App.js
+++ src/App.js
@@ -5,11 +5,7 @@ import NavBar from "./components/NavBar";
 import { BrowserRouter, Route, Routes  } from 'react-router-dom';
 
 function App() {
-  
   return (
-    
-    
-    
     
     
     <BrowserRouter>
